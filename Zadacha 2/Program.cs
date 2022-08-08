5a05c4753ee2c2f50208fdd62b8bc6505f01e6b1
DZMITRY <sharamet.dmitri@outlook.com>
2022-08-08 13:44:44 +0300

    Задача 2 Новая версия

--- Zadacha 2/Program.cs
+++ Zadacha 2/Program.cs
@@ -1,34 +1,39 @@
-﻿
-Console.Clear();
+﻿Console.Clear();
  
     {
- 
         int a, b;
  
-        Console.WriteLine("Введите два числа : ");
+        Console.WriteLine("Введите два числа: ");
  
         a = Convert.ToInt32(Console.ReadLine());
  
         b = Convert.ToInt32(Console.ReadLine());
- 
+        
         if (a > b)
  
         {
- 
-            Console.WriteLine("{0} наибольшее число", a);
- 
+        Console.WriteLine("{0} наибольшее число", a);
         }
  
         else
  
         {
- 
             Console.WriteLine("{0} наибольшее число ", b);
- 
         }
- 
+
+        if (a < b)
+
+        {
+            Console.WriteLine("{0} наименьшее число", a);
+        }
+
+        else
+
+        {
+            Console.WriteLine("{0} наименьшее число", b);
+        }
+
         Console.ReadLine();
- 
     }
 
 
