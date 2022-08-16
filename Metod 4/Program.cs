55b8db90f734f2ae3b4486796c4566d0bc62143c
DZMITRY <sharamet.dmitri@outlook.com>
2022-08-16 22:07:01 +0300

    Метод 4

---
+++ Metod 4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Вид метода 4 (что-то возращают и что-то принимают)
+
+string Method4(int count, string test)
+{
+   int i = 0;
+   string result = String.Empty;
+
+   while (i < count)
+   {
+     result = result + test;
+     i++;
+   }
+   return result;
+}
+string res = Method4(10, "z");
+Console.WriteLine(res);
