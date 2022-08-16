91ece806e0f845ce0fafd63b5db6cbaadd23f927
DZMITRY <sharamet.dmitri@outlook.com>
2022-08-16 22:24:12 +0300

    Метод 4.1 через for

---
+++ Metod 4.1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Вид метода 4 (что-то возращают и что-то принимают)
+
+string Method4(int count, string test)
+{
+   string result = String.Empty;
+   for(int i = 0; i < count; i++)
+   {
+     result = result + test;
+   }
+   return result;
+}
+string res = Method4(10, "z");
+Console.WriteLine(res);
